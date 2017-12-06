0be1dfa8644c7839e0e548def72c1eab3322e3dd
zhangchao <zhangc0804@163.com>
2017-12-06 09:43:05 +0800

    <url-pattern>路径匹配规则研究，按优先级遵循以下规则： 1、精确匹配，如/exactServlet 2、通配符（前缀）匹配，如/* 3、扩展名匹配，如*.do 4、资源文件匹配，如欢迎页面的匹配 5、默认匹配，除以上之外的匹配

---
+++ webapp-maven-template/src/main/java/com/oldox/examples/servlet/DefaultServlet.java
@@ -0,0 +1,40 @@
+package com.oldox.examples.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * 这是一个简单的servlet，用于演示servlet的生命周期
+ * <p>
+ * 
+ * @author ZhangChao
+ *
+ */
+public class DefaultServlet extends HttpServlet {
+	Logger logger = LoggerFactory.getLogger(DefaultServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		pw.write("This is: "+this.getClass()+"\n");
+		pw.write("ContextPath: "+req.getContextPath()+"\n");
+		pw.write("ServletPath: "+req.getServletPath()+"\n");
+	}
+
+}
